35059e180ec10edf6fee22d8011f7176c70896af
ram <ramusus@gmail.com>
2013-01-28 15:57:22 +0530

    added attributes by default to GenericRelationListFilter

--- vkontakte_api/admin.py
+++ vkontakte_api/admin.py
@@ -6,6 +6,13 @@ admin.ModelAdmin.list_per_page = 50
 
 class GenericRelationListFilter(admin.SimpleListFilter):
     separator = '-'
+    ct_field_name = ''
+    id_field_name = ''
+    field_name = ''
+
+    @property
+    def parameter_name(self):
+        return self.field_name
 
     def lookups(self, request, model_admin):
         return [('%s%s%s' % (getattr(instance, self.ct_field_name).id, self.separator, getattr(instance, self.id_field_name)), getattr(instance, self.field_name)) for instance in model_admin.model.objects.order_by().distinct(self.ct_field_name,self.id_field_name)]
