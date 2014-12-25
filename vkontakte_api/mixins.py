403e8053b4c36f4072c9b44b795df92e06e031a7
ramusus <ramusus@gmail.com>
2014-12-25 15:48:46 +0700

    added AuthorableModelMixin, LikableModelMixin

--- vkontakte_api/mixins.py
+++ vkontakte_api/mixins.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
+import logging
+
 from django.contrib.contenttypes import generic
 from django.contrib.contenttypes.models import ContentType
-from django.db import models
-from . models import VkontakteManager, VkontakteTimelineManager
+from django.db import models, transaction
+from m2m_history.fields import ManyToManyHistoryField
+from vkontakte_users.models import User
+
+from .models import VkontakteManager, VkontakteTimelineManager
+
+log = logging.getLogger('vkontakte_api')
 
 
 class CountOffsetManagerMixin(VkontakteManager):
@@ -42,8 +49,21 @@ class AfterBeforeManagerMixin(VkontakteTimelineManager):
         return super(AfterBeforeManagerMixin, self).fetch(**kwargs)
 
 
+class AuthorableModelMixin(models.Model):
+
+    author_content_type = models.ForeignKey(
+        ContentType, null=True, related_name='content_type_authors_vkontakte_%(class)ss')
+    author_id = models.BigIntegerField(null=True, db_index=True)
+    author = generic.GenericForeignKey('author_content_type', 'author_id')
+
+    class Meta:
+        abstract = True
+
+
 class OwnerableModelMixin(models.Model):
-    owner_content_type = models.ForeignKey(ContentType, null=True, related_name='content_type_owners_%(class)ss')
+
+    owner_content_type = models.ForeignKey(
+        ContentType, null=True, related_name='content_type_owners_vkontakte_%(class)ss')
     owner_id = models.BigIntegerField(null=True, db_index=True)
     owner = generic.GenericForeignKey('owner_content_type', 'owner_id')
 
@@ -63,13 +83,53 @@ class OwnerableModelMixin(models.Model):
         return object
 
     @property
-    def remote_owner_id(self):
-        if self.owner_content_type.model == 'user':
-            return self.owner_id
+    def owner_remote_id(self):
+        return self.get_owner_remote_id(self.owner)
+
+    @classmethod
+    def get_owner_remote_id(cls, owner):
+        if owner._meta.module_name == 'user':
+            return owner.remote_id
+        elif owner._meta.module_name == 'group':
+            return -1 * owner.remote_id
         else:
-            return -1 * self.owner_id
+            raise ValueError("Field owner should store User of Group")
 
     def parse(self, response):
         self.owner = self._get_or_create_group_or_user(response.pop('owner_id'))
-
         super(OwnerableModelMixin, self).parse(response)
+
+
+class LikableModelMixin(models.Model):
+
+    likes_users = ManyToManyHistoryField(User, related_name='like_%(class)ss')
+    likes_count = models.PositiveIntegerField(u'Likes', null=True)
+
+    class Meta:
+        abstract = True
+
+    @property
+    def likes_remote_type(self):
+        raise NotImplementedError()
+
+    @transaction.commit_on_success
+    def fetch_likes(self, *args, **kwargs):
+
+        kwargs['likes_type'] = self.likes_remote_type
+        kwargs['item_id'] = self.remote_id
+        kwargs['owner_id'] = self.owner_remote_id
+
+        log.debug('Fetching likes of %s %s of owner "%s"' % (self._meta.module_name, self.remote_id, self.owner))
+
+        self.likes_users = User.remote.fetch_instance_likes(self, m2m_field_name='likes_users', *args, **kwargs)
+
+        # update self.likes_count
+        likes_count = self.likes_users.count()
+        if likes_count < self.likes_count:
+            log.warning('Fetched ammount of like users less, than attribute `likes` of post "%s": %d < %d' % (
+                self.remote_id, likes_count, self.likes_count))
+        elif likes_count > self.likes_count:
+            self.likes_count = likes_count
+            self.save()
+
+        return self.likes_users.all()
