073b6b73eea40c99c776b4799ebd6119915771c9
ramusus <ramusus@gmail.com>
2015-06-09 18:27:34 +0300

    updated factory

--- vkontakte_api/factories.py
+++ vkontakte_api/factories.py
@@ -2,9 +2,11 @@ import factory
 
 
 class DjangoModelNoCommitFactory(factory.DjangoModelFactory):
-    ABSTRACT_FACTORY = True
 
     @classmethod
     def _create(cls, *args, **kwargs):
         kwargs['commit_remote'] = False
         return super(DjangoModelNoCommitFactory, cls)._create(*args, **kwargs)
+
+    class Meta:
+        abstract = True
